14d4702f09c3c6bab4f46814ddca3966f04e532c
Zlendy <zlendy@proton.me>
2024-05-08 14:02:38 +0200

    refactor: Redo Footer markup and styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
-
+import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    icon(),
     vue(),
     tailwind({
       applyBaseStyles: false,
