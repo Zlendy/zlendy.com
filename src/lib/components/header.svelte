ff6e32aa4dbf86b6c1ea9fdaa573b386b76f16f1
Zlendy <zlendy@proton.me>
2024-07-26 23:02:51 +0200

    fix: position sticky in Header component does not show the element's true location in screen

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -6,7 +6,7 @@
 	import { page } from '$app/stores';
 </script>
 
-<header class="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+<header class="top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
 	<nav
 		class="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6"
 	>
