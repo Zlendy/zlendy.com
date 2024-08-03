a54ad81aa15d4d56f623eb0e2efb38cfcf500522
Zlendy <zlendy@proton.me>
2024-08-03 08:38:50 +0200

    refactor: Change subtitle color in /

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,5 @@
 	class="relative z-10 flex h-full min-h-screen flex-col items-center justify-center text-center"
 >
 	<h1 class="mb-4 text-5xl font-bold leading-tight">Zlendy</h1>
-	<p class="mb-8 text-lg text-gray-300">Work in Progress</p>
+	<p class="mb-8 text-lg">Work in Progress</p>
 </div>
