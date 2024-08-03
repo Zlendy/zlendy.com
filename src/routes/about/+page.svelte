09bfd874041ab4c3e66dcb00db2bf69ced95400a
Zlendy <zlendy@proton.me>
2024-08-03 08:32:39 +0200

    refactor: Slight modification to subtitle in /about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,6 +1,6 @@
 <div
 	class="relative z-10 flex h-full min-h-screen flex-col items-center justify-center text-center"
 >
-	<h1 class="mb-4 text-5xl font-bold leading-tight">Zlendy</h1>
-	<p class="mb-8 text-lg text-gray-300">TODO: About</p>
+	<h1 class="mb-4 text-5xl font-bold leading-tight">About</h1>
+	<p class="mb-8 text-lg">TODO</p>
 </div>
