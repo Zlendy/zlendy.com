ad935bc803190fb25836db24cecedce9cd2a2c2d
Zlendy <zlendy@proton.me>
2024-09-02 22:34:14 +0200

    feat: Improve layout of posts in /blog

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -5,11 +5,11 @@
 </script>
 
 <article>
-	<header>
-		<h1>{data.post.title}</h1>
+	<header class="relative z-10 flex min-h-48 flex-col items-center justify-center text-center">
+		<h1 class="mb-4 text-5xl font-bold leading-tight">{data.post.title}</h1>
 	</header>
 	<!-- render the post -->
-	<div>
+	<div class="mx-[10vw]">
 		<svelte:component this={data.component} />
 	</div>
 </article>
