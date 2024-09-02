436faa13d82f283a5af53329cf6c841f1d2dfe9d
Zlendy <zlendy@proton.me>
2024-09-02 22:14:45 +0200

    feat: Improve style of error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,6 +1,4 @@
-<script lang="ts">
-	import { page } from '$app/stores';
-</script>
-
-<h1>ERROR</h1>
-<p>{$page.error?.message}</p>
+<div class="relative z-10 flex h-screen flex-col items-center justify-center text-center">
+	<h1 class="mb-4 text-5xl font-bold leading-tight">ERROR</h1>
+	<p class="mb-8 text-lg">Page not found</p>
+</div>
