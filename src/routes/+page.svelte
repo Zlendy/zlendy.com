54c934e9a86690d8cd267dbb43c77dae620fae58
Zlendy <zlendy@proton.me>
2024-11-16 10:30:51 +0100

    feat: Invert colors of logo when the color mode is not dark

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
+<script lang="ts">
+	import { mode } from 'mode-watcher';
+</script>
+
 <div class="relative z-10 flex h-screen flex-col items-center justify-center text-center">
-	<img src="/favicon.png" alt="logo" />
+	<img src="/favicon.png" alt="logo" class:invert={$mode !== 'dark'} />
 	<h1 class="mb-4 text-5xl font-bold leading-tight">Zlendy</h1>
 	<p class="mb-8 text-lg">Work in Progress</p>
 </div>
