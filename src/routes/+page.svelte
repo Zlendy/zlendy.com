958d37d83dfda0a28d5c845dc3bda04d9a4269dc
Zlendy <zlendy@proton.me>
2024-11-16 08:15:47 +0100

    feat: Add website logo to index page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <div class="relative z-10 flex h-screen flex-col items-center justify-center text-center">
+	<img src="/favicon.png" alt="logo" />
 	<h1 class="mb-4 text-5xl font-bold leading-tight">Zlendy</h1>
 	<p class="mb-8 text-lg">Work in Progress</p>
 </div>
