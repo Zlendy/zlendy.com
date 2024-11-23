ee9606a718cb4dfd18c3c95111831b48735a2756
Zlendy <zlendy@proton.me>
2024-11-23 15:43:45 +0100

    refactor: Rename <HoverTitle /> to <LinkHoverTitle />

--- src/lib/components/link-arrow.svelte
+++ src/lib/components/link-arrow.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import { SquareArrowOutUpRight } from 'lucide-svelte';
-	import HoverTitle from './hover-title.svelte';
+	import LinkHoverTitle from './link-hover-title.svelte';
 </script>
 
-<HoverTitle
+<LinkHoverTitle
 	class="relative px-1
 	before:pointer-events-none before:absolute before:left-0 before:right-0 before:top-[calc(100%-0.25rem)] before:h-1 before:bg-foreground/25 before:transition-all
 	hover:before:top-0 hover:before:h-full"
@@ -11,4 +11,4 @@
 >
 	<slot></slot>
 	<SquareArrowOutUpRight class="inline h-[1em] w-[1em]" />
-</HoverTitle>
+</LinkHoverTitle>
