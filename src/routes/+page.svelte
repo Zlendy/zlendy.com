9535ae8cd8f83812057dba793f17abaf083e5b27
Zlendy <zlendy@proton.me>
2024-11-24 16:20:04 +0100

    fix: Website logo in / appears with the wrong color for an instant

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,14 @@
 </script>
 
 <div class="relative z-10 flex h-screen flex-col items-center justify-center text-center">
-	<img src="/favicon.png" alt="logo" class:invert={$mode !== 'dark'} />
+	<img
+		src="/favicon.png"
+		width="192"
+		height="192"
+		alt="logo"
+		class:invisible={$mode === undefined}
+		class:invert={$mode !== 'dark'}
+	/>
 	<h1 class="mb-4 text-5xl font-bold leading-tight">Zlendy</h1>
 	<p class="mb-8 text-lg">Work in Progress</p>
 </div>
