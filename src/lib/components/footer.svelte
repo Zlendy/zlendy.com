349e75f3e2a5c1d271f36c4ae9e7afa9ce063569
Zlendy <zlendy@proton.me>
2024-11-24 18:53:36 +0100

    chore: Add license

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -2,13 +2,19 @@
 	import ModeToggle from '$lib/components/modetoggle.svelte';
 	import Logo from '$lib/components/logo.svelte';
 	import LinkHoverTitle from './link-hover-title.svelte';
+	import LinkArrow from './link-arrow.svelte';
 </script>
 
 <footer class="border-t">
 	<div
 		class="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0"
 	>
-		<p>zlendy.com © 2024 Zlendy.</p>
+		<p>
+			zlendy.com © 2024 Zlendy.
+			<LinkArrow href="https://github.com/Zlendy/zlendy.com/blob/main/LICENSE.md">
+				Read the license
+			</LinkArrow>
+		</p>
 		<div class="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
 			<div class="flex flex-wrap gap-4">
 				<LinkHoverTitle title="ActivityPub" rel="me" href="https://social.zlendy.com/@zlendy">
