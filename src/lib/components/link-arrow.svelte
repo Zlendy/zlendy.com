a86348e84947ee368868e871e7fcbe7e8ba719b0
Zlendy <zlendy@proton.me>
2024-11-25 21:48:34 +0100

    feat: Open external links in a new page, show which links are external with an arrow

--- src/lib/components/link-arrow.svelte
+++ src/lib/components/link-arrow.svelte
@@ -1,13 +1,24 @@
 <script lang="ts">
 	import { SquareArrowOutUpRight } from 'lucide-svelte';
 	import LinkHoverTitle from './link-hover-title.svelte';
+	import type { HTMLAnchorAttributes } from 'svelte/elements';
+	import { getAnchorTarget, isSameOrigin } from './link';
+
+	interface $$Props extends HTMLAnchorAttributes {}
+
+	export let href: HTMLAnchorAttributes['href'] = undefined;
+	const sameorigin = isSameOrigin(href);
 </script>
 
 <LinkHoverTitle
 	class="bg-[linear-gradient(hsl(var(--foreground)/25%),hsl(var(--foreground)/25%))] bg-[length:100%_0.25rem] bg-[0%_100%] bg-no-repeat
 	px-1 transition-all hover:bg-[length:100%_100%]"
 	{...$$restProps}
+	{href}
+	target={getAnchorTarget(sameorigin)}
 >
 	<slot></slot>
-	<SquareArrowOutUpRight class="inline h-[1em] w-[1em]" />
+	{#if !sameorigin}
+		<SquareArrowOutUpRight class="inline h-[1em] w-[1em]" />
+	{/if}
 </LinkHoverTitle>
---
+++ src/lib/components/link.ts
@@ -0,0 +1,13 @@
+import { browser } from '$app/environment';
+import type { HTMLAnchorAttributes } from 'svelte/elements';
+
+export function isSameOrigin(href: HTMLAnchorAttributes['href']) {
+	if (!browser) return true; // Avoid SSR errors in dev mode
+	if (!href) return false;
+	if (!href.startsWith('http')) return true; // This is a relative route
+	return href.startsWith(window.location.origin);
+}
+
+export function getAnchorTarget(sameorigin: boolean): HTMLAnchorAttributes['target'] {
+	return sameorigin ? '_self' : '_blank';
+}
