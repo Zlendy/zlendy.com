ceff964cb701cc25a0cffcb35bfc444cb35061f4
Zlendy <zlendy@proton.me>
2024-12-01 23:36:06 +0100

    feat: Allow customization of <Logo /> using CSS classes

--- src/lib/components/logo.svelte
+++ src/lib/components/logo.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
+	import type { HTMLBaseAttributes } from 'svelte/elements';
+
+	interface $$Props extends HTMLBaseAttributes {
+		name: string;
+	}
+
 	export let name: string;
+	let className: HTMLBaseAttributes['class'] = undefined;
+	export { className as class };
 </script>
 
 <i
-	class="inline-block h-[1em] w-[1em] bg-foreground"
+	class="inline-block h-[1em] w-[1em] bg-foreground {className}"
 	style="mask: url(/icons/logos/{name}.svg) no-repeat center;"
+	{...$$restProps}
 ></i>
