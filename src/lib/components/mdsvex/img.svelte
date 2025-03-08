e25e7fadb5be2d1b1a20080fce6433c7ad2d06b1
Zlendy <zlendy@proton.me>
2025-03-08 11:09:21 +0100

    fix: img.svelte crashes on dev mode

--- src/lib/components/mdsvex/img.svelte
+++ src/lib/components/mdsvex/img.svelte
@@ -2,15 +2,16 @@
 	import type { HTMLImgAttributes } from 'svelte/elements';
 	import ImageZoom from '../image-zoom.svelte';
 
-	
-
-	interface Props {
+	interface Props extends HTMLImgAttributes {
 		alt?: HTMLImgAttributes['alt'];
-		[key: string]: any
 	}
 
 	let { alt = undefined, ...rest }: Props = $props();
 </script>
+
+<!-- As of 2025-03-08, this component crashes when mdsvex renders and HMR is enabled it unless I insert any element before <figure> -->
+<!-- What the F$@% -->
+<span></span>
 
 <figure class="w-fit py-4">
 	<ImageZoom class="max-h-96 w-full max-w-96 object-contain" {...rest} {alt} />
