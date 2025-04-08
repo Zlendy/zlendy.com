Zlendy/zlendy.com

commit 2780f5a0467f4f24807093b5afbdcd708a045c4a

fix: <LineArrow> shows white space in "sameorigin" links

src/lib/components/link-arrow.svelte
@@ -13,8 +13,8 @@
 </script>
 
 <LinkHoverTitle
-	class="bg-[linear-gradient(hsl(var(--foreground)/25%),hsl(var(--foreground)/25%))] bg-[length:100%_0.25rem] bg-[0%_100%] bg-no-repeat
-	px-1 transition-all hover:bg-[length:100%_100%]"
+	class="inline-block bg-[linear-gradient(hsl(var(--foreground)/25%),hsl(var(--foreground)/25%))] bg-[length:100%_0.25rem] bg-[0%_100%]
+	bg-no-repeat px-1 transition-all hover:bg-[length:100%_100%]"
 	{...rest}
 	{href}
 	target={getAnchorTarget(sameorigin)}

commit 1ec302f136097c16ef418b22bd9e57f70f223fa8

fix: <LineArrow> breaks link underlines, underline is applied to the entire element instead of each line
Bug was introduced in 2780f5a0467f4f24807093b5afbdcd708a045c4a

src/lib/components/link-arrow.svelte
@@ -13,14 +13,13 @@
 </script>
 
 <LinkHoverTitle
-	class="inline-block bg-[linear-gradient(hsl(var(--foreground)/25%),hsl(var(--foreground)/25%))] bg-[length:100%_0.25rem] bg-[0%_100%]
+	class="bg-[linear-gradient(hsl(var(--foreground)/25%),hsl(var(--foreground)/25%))] bg-[length:100%_0.25rem] bg-[0%_100%]
 	bg-no-repeat px-1 transition-all hover:bg-[length:100%_100%]"
 	{...rest}
 	{href}
 	target={getAnchorTarget(sameorigin)}
 >
-	{@render children?.()}
-	{#if !sameorigin}
-		<SquareArrowOutUpRight class="inline h-[1em] w-[1em]" />
+	{@render children?.()}{#if !sameorigin}
+		&nbsp;<SquareArrowOutUpRight class="inline h-[1em] w-[1em]" />
 	{/if}
 </LinkHoverTitle>