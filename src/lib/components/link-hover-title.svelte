431f3f328947b180cd6894719c95366928eeb780
Zlendy <zlendy@proton.me>
2025-04-25 20:43:37 +0200

    feat: Track outbound link clicks
    https://umami.is/docs/track-outbound-links

--- src/lib/components/link-hover-title.svelte
+++ src/lib/components/link-hover-title.svelte
@@ -1,16 +1,22 @@
 <script lang="ts">
 	import * as HoverCard from '$lib/components/ui/hover-card';
 	import type { ComponentProps, Snippet } from 'svelte';
+	import { isSameOrigin } from './link';
 
 	interface Props extends ComponentProps<typeof HoverCard.Trigger> {
 		children?: Snippet;
 	}
 
-	let { title = undefined, children, ...rest }: Props = $props();
+	let { title = undefined, children, href, ...rest }: Props = $props();
 </script>
 
 <HoverCard.Root>
-	<HoverCard.Trigger {...rest}>
+	<HoverCard.Trigger
+		data-umami-event={isSameOrigin(href) ? undefined : 'outbound-link-click'}
+		data-umami-event-url={href}
+		{href}
+		{...rest}
+	>
 		{@render children?.()}
 	</HoverCard.Trigger>
 	{#if title}
