f7bfbbfbfb07366e7c794acbaace7f4b158bd84b
Zlendy <zlendy@proton.me>
2025-05-02 17:28:45 +0200

    fix: Blog articles get ommited in /blog.json if their "fediverse" value is undefined

--- src/routes/blog.json/+server.ts
+++ src/routes/blog.json/+server.ts
@@ -7,10 +7,10 @@ export const prerender = true;
  * Links a blog post slug to a fediverse ID
  */
 export function GET() {
-	const map = new Map<string, string | undefined>();
+	const map = new Map<string, string | null>();
 
 	for (const post of posts) {
-		map.set(post.slug, post.fediverse);
+		map.set(post.slug, post.fediverse ?? null);
 	}
 
 	return json(Object.fromEntries(map));
