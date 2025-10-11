930c474050c8d00f766504833f71e5ca14d55c96
Zlendy <zlendy@proton.me>
2025-10-11 19:45:56 +0200

    refactor: Reduce size of the headings

--- src/lib/components/heading.svelte
+++ src/lib/components/heading.svelte
@@ -5,12 +5,12 @@
 	import type { Snippet } from 'svelte';
 
 	const styles = {
-		h1: 'text-5xl',
-		h2: 'text-4xl',
-		h3: 'text-3xl',
-		h4: 'text-2xl',
-		h5: 'text-xl',
-		h6: 'text-md'
+		h1: 'text-4xl',
+		h2: 'text-3xl',
+		h3: 'text-2xl',
+		h4: 'text-xl',
+		h5: 'text-lg',
+		h6: 'text-base'
 	} as const;
 
 	interface Props {
